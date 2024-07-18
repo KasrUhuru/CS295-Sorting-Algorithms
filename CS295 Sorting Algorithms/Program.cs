27f440eaf77647bdbd944df1afd3ef802f5b8f70
mrivera <matias.g.rivera.civ@gmail.com>
2024-07-18 15:56:05 -0700

    Added Method Overloads and started interactivity interface. Starting MergeSort method.

--- CS295 Sorting Algorithms/Program.cs
+++ CS295 Sorting Algorithms/Program.cs
@@ -31,6 +31,62 @@ namespace SortingAlgorithms
             InsertionSort(arr1);
             stopwatch.Stop();
             Console.WriteLine(stopwatch.Elapsed.ToString());
+
+            /// For the Student Class
+            Console.WriteLine("Please select a sorting algorithm:");
+            Console.WriteLine("1: Bubble Sort");
+            Console.WriteLine("2: Selection Sort");
+            Console.WriteLine("3: Insertion Sort");
+
+
+            string? userSelection = Console.ReadLine();
+
+            //Student student1 = new Student();
+            //student1.name = "Melissa";
+            //student1.gpa = 3.0;
+
+            Student student1 = new Student("Jill Stein", 4.0);
+
+            Student student2 = new Student("Matias", 3.0);
+
+            Student student3 = new Student("Matt", 3.9);
+
+            Student student5 = new Student("Carlos", 2.4);
+
+            Student student4 = new Student("Trump", 0.1);
+
+            Student[] students = { student1, student2, student3, student4, student5 };
+
+            // Doesn't work! Constructor doesn't permit non-named Student objects
+            // Student student3 = new Student(4.0);
+
+            // Utilize a SWITCH statement to handle different possibilities
+            switch  (userSelection)
+            {
+                case "1":
+                    // Commit Bubble activities
+                    Console.WriteLine("Initiating Bubble Sort...");
+                    BubbleSort(students);
+                    break;
+                case "2":
+                    // Commit Selection activities
+                    Console.WriteLine("Initiating Selection Sort...");
+
+                    break;
+                case "3":
+                    // Commit Insertion activities
+                    Console.WriteLine("Initiating Insertion Sort...");
+                    break;
+                default:
+                    // none of the cases matched
+                    break;
+            }
+
+            PrintStudentArray(students);
+
+            MergeSort(arr2);
+            PrintArray(arr2);
+
         }
 
         public static void PrintArray(int[] arrToSort)
@@ -38,6 +94,15 @@ namespace SortingAlgorithms
             foreach (int element in arrToSort)
             {
                 Console.Write($"{element} ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintStudentArray(Student[] arrToSort)
+        {
+            foreach (var item in arrToSort)
+            {
+                Console.WriteLine($"{item.name}: {item.gpa} ");
             }
             Console.WriteLine();
         }
@@ -68,6 +133,35 @@ namespace SortingAlgorithms
             }
 
             return totalOuterIterations;
+        }
+
+        public static void BubbleSort(Student[] arrToSort)
+        {
+            int totalOuterIterations = 0;
+            // Change from INT to STUDENT class for the temp variable
+            Student temp;
+            // Overall O(n^2) runtime - Big O
+            // Big Omega - O(n^2)
+            for (int i = 0; i < arrToSort.Length - 1; i++) // O(n) how many times we need to go though the unsorted array
+            {
+                totalOuterIterations++;
+                int swaps = 0;
+                for (int j = 0; j < arrToSort.Length - 1 - i; j++)  // O(n)
+                {
+                    // we need to swap SWITCHED FROM ASCENDING TO DESCENDING
+                    if (arrToSort[j].gpa < arrToSort[j + 1].gpa)           // can modify if '>' change to '<'
+                    {
+                        swaps++;
+                        temp = arrToSort[j];
+                        arrToSort[j] = arrToSort[j + 1];
+                        arrToSort[j + 1] = temp;
+                    }
+                }
+
+                if (swaps == 0) break; // break out of the for loop
+            }
+
+
         }
 
         public static void SelectionSort(int[] arrToSort)
@@ -118,5 +212,33 @@ namespace SortingAlgorithms
                 arrToSort[priorIndex + 1 ] = temp;
             }
         }
+
+        // Recursive function that splits the array up and merges it together
+        public static void MergeSort(int[] arrToSort)
+        {
+            if (arrToSort.Length <= 1) return; // Example of early return
+
+            int mid = arrToSort.Length / 2;
+
+            // Create left sub-array to hold left of the midpoint
+            int[] leftSubArray = new int[mid];
+
+            int[] rightSubArray = new int[arrToSort.Length - mid];
+
+            for (int i = 0; i < mid; i++)
+            {
+                leftSubArray[i] = arrToSort[i];
+            }
+
+            for (int i = mid; i < arrToSort.Length; i++)
+            {
+                rightSubArray[i - mid] = arrToSort[i];
+            }
+
+            MergeSort(leftSubArray);
+            MergeSort(rightSubArray);
+            
+        }
+
     }
 }
---
+++ CS295 Sorting Algorithms/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public class Student
+    {
+        public string name;
+        public double gpa;
+
+        // Construct a student with only a name
+        public Student(string name)
+        {
+            this.name = name;
+        }
+
+        //Construct a student with name and GPA
+        public Student(string name, double gpa)
+        {
+            this.name = name;
+            this.gpa = gpa;
+        }
+    }
+}
